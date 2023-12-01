73ef97970f239ee7226923ae5182c40a94d27bdd
Stanislav2022 <tsv5@ukr.net>
2023-12-02 00:18:33 +0200

    1

--- my-app/src/components/SharedLayout/SharedLayout.jsx
+++ my-app/src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import Footer
 import { Container, Header, Logo, Link } from "./SharedLayout.styled";
 
 export const SharedLayout = () => {
