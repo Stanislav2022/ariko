a0fb3a5f384d4027d3cb8086a0da63233dcb9156
Stanislav2022 <tsv5@ukr.net>
2023-12-01 23:53:40 +0200

    1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "modern-normalize";
 import "./index.css";
 import App from "./components/App.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
