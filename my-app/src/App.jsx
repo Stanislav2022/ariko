01c9c8ef3152ab16879db072230f24ed6553003e
Stanislav2022 <tsv5@ukr.net>
2023-12-01 22:11:41 +0200

    pages

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,16 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+import styled from "styled-components";
 import Home from "path/to/pages/Home";
-import About from "path/to/pages/About";
+import Contacts from "path/to/pages/Contacts";
 import Products from "path/to/pages/Products";
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
 
 export const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
+      <div>
+        <nav>
+          <StyledLink to="/" end>
+            Home
+          </StyledLink>
+          <StyledLink to="/contacts">Contacts</StyledLink>
+          <StyledLink to="/products">Products</StyledLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
